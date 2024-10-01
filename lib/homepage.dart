74afac221b1e38b4a43a46abdba300dd6f915d2d
nabeel734 <sng22mca57@sngce.ac.in>
2024-10-02 00:49:41 +0530

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+class homePage extends StatelessWidget {
+  homePage({super.key});
+
+   @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.blue.shade300,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+
+                
+
+              ]
+          )
+        )
+      )
+    );
+  }
+
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:my_remainder/homepage.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PAY N TRACK',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SplashScreen(), // Set the initial route to the splash screen
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      // Navigate to the home screen after 3 seconds
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (BuildContext context) => homePage(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade500, // Change the background color
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'My Hostel',
+              style: TextStyle(
+                fontSize: 44,
+                fontWeight: FontWeight.bold,
+                color: Colors.white, // Change the text color
+              ),
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white), // Change the progress indicator color
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Welcome to PAY N TRACK!'),
+      ),
+    );
+  }
+}
